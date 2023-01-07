d6ed18df28986a8d9cc6c08f831d015835a14951
paulopariz <paulopariz01@gmail.com>
2023-01-07 18:55:14 -0300

    add tailwind

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/axios'
+import './assets/css/main.css'
 
 Vue.config.productionTip = false
 
