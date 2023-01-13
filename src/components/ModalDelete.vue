c72ebb2be7539e74c1185f0a97a0cad621c41b0c
paulopariz <paulopariz01@gmail.com>
2023-01-13 19:51:11 -0300

    component ModalDelete

---
+++ src/components/ModalDelete.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <input type="checkbox" id="my-modal" class="modal-toggle" />
+    <div class="modal">
+      <div class="modal-box rounded-none">
+        <h1 class="font-semibold text-xl text-center">Tem certeza que deseja ecluir?</h1>
+
+        <div class="modal-action justify-center mt-14">
+          <label @click="deletar" for="my-modal" class="btn">NÃO, CANCELAR</label>
+          <label
+            @click="deletar"
+            for="my-modal"
+            class="btn bg-red-600 border-none hover:bg-red-700"
+            >SIM</label
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalDelete",
+
+  methods: {
+    deletar() {
+      this.$http.delete(`deletar/${this.$route.params.id}`).then((response) => {
+        if (response.data == "success") {
+          setTimeout(() => {
+            this.$swal("Sucesso", "Link deletado com sucesso", "success");
+            this.$router.push("/listar");
+          }, 1000);
+        }
+      });
+    },
+  },
+};
+</script>
--- src/components/RedirectEdit.vue
+++ src/components/RedirectEdit.vue
@@ -106,75 +106,47 @@
     </div>
 
     <!--MODAL DELETE-->
-    <input type="checkbox" id="my-modal" class="modal-toggle" />
-    <div class="modal">
-      <div class="modal-box rounded-none">
-        <h1 class="font-semibold text-xl text-center">Tem certeza que deseja ecluir?</h1>
-
-        <div class="modal-action justify-center mt-14">
-          <label @click="deletar" for="my-modal" class="btn">NÃO, CANCELAR</label>
-          <label
-            @click="deletar"
-            for="my-modal"
-            class="btn bg-red-600 border-none hover:bg-red-700"
-            >SIM</label
-          >
-        </div>
-      </div>
-    </div>
+    <ModalDelete />
   </div>
 </template>
 
 <script>
+import ModalDelete from './ModalDelete.vue';
+
 export default {
-  name: "RedirectEdit",
-
-  data() {
-    return {
-      nome: "",
-      email: "",
-    };
-  },
-
-  methods: {
-    getUser() {
-      this.$http.get(`editar/${this.$route.params.id}`).then((response) => {
-        this.nome = response.data.nome;
-        this.email = response.data.email;
-      });
+    name: "RedirectEdit",
+    data() {
+        return {
+            nome: "",
+            email: "",
+        };
     },
-
-    update() {
-      var data = {
-        name: this.nome,
-        email: this.email,
-      };
-
-      this.$http.put(`atualizar/${this.$route.params.id}`, data).then((response) => {
-        if (response.data == "success") {
-          setTimeout(() => {
-            this.$swal("Sucesso", "Link editado com sucesso", "success");
-
-            this.$router.push("/listar");
-          }, 1300);
-        }
-      });
+    methods: {
+        getUser() {
+            this.$http.get(`editar/${this.$route.params.id}`).then((response) => {
+                this.nome = response.data.nome;
+                this.email = response.data.email;
+            });
+        },
+        update() {
+            var data = {
+                name: this.nome,
+                email: this.email,
+            };
+            this.$http.put(`atualizar/${this.$route.params.id}`, data).then((response) => {
+                if (response.data == "success") {
+                    setTimeout(() => {
+                        this.$swal("Sucesso", "Link editado com sucesso", "success");
+                        this.$router.push("/listar");
+                    }, 1300);
+                }
+            });
+        },
+        
     },
-
-    deletar() {
-      this.$http.delete(`deletar/${this.$route.params.id}`).then((response) => {
-        if (response.data == "success") {
-          setTimeout(() => {
-            this.$swal("Sucesso", "Link deletado com sucesso", "success");
-            this.$router.push("/listar");
-          }, 1000);
-        }
-      });
+    created() {
+        this.getUser();
     },
-  },
-
-  created() {
-    this.getUser();
-  },
+    components: { ModalDelete }
 };
 </script>
